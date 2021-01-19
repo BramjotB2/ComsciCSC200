913504d3a2c2d214717a3c016fdf6afc4ceae33a
Bramjot Bansal <bramban1313@gmail.com>
2021-01-19 11:50:34 -0500

    Add project files.

---
+++ BramjotBansal_hd8832_HW1_B.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int length = 5;
+    int width = 6;
+    int area = 0;
+
+
+    cout << "The Length is " << length << endl;
+    cout << "The Width is " << width << endl;
+    cout << "The original area is " << area << endl;
+    area = length * width;
+    cout << "The area with a length of 5 and a width of 6 is " << area << endl;
+}
+
+
+
+
+
